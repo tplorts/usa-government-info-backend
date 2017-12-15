015649e6a508b7fd4626a42445bd6a2bcc3eb28f
Ted Lorts <tplorts@gmail.com>
2017-12-14 19:30:18 -0800

    enabled update on boot with env var

--- server/boot/03b-update-data.js
+++ server/boot/03b-update-data.js
@@ -6,10 +6,15 @@ module.exports = async function (app, next) {
   log('________________________________')
   log('update legislator data if needed')
 
+  const EnvUpdate = process.env.UPDATE_LEGISLATORS_ON_BOOT
+  const isUpdateEnabled = !!(EnvUpdate && EnvUpdate === 'true')
+
   const { Legislator, Senator, Representative } = app.models
 
   try {
-    await Promise.all([ Senator, Representative ].map(Model => Legislator.updateIfNeeded(Model)))
+    if (isUpdateEnabled) {
+      await Promise.all([ Senator, Representative ].map(Model => Legislator.updateIfNeeded(Model)))
+    }
     next()
   } catch (err) {
     next(err)
